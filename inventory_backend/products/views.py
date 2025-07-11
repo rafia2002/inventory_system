06788ac0854a9d2ed2a4c6874906dc50a2f9f584
rafia2002 <rzmukti2002@gmail.com>
2025-07-11 23:23:03 +0600

    first commit

---
+++ inventory_backend/products/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializers
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializers
+
+    
+# Create your views here.
